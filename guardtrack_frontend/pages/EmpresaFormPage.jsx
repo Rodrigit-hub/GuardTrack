9f940e2b66ef60525ffb5fa72c6ef60b761a4e05
Rodrigit-hub <rodri98alves@gmail.com>
2024-09-20 20:30:22 -0300

    finalizando EmpresaFormPage

--- guardtrack_frontend/pages/EmpresaFormPage.jsx
+++ guardtrack_frontend/pages/EmpresaFormPage.jsx
@@ -1,7 +1,27 @@
+import {userForm} from "react-hook-form"
 
 export function EmpresaFormPage() {
+
+    const {register, handleSubmit} = userForm()
+
+    const onSubmit = handleSubmit(data => {
+      console.log(data)
+    })
+
     return (
-      <div>EmpresaFormPage</div>
+      <div>
+        <form onSubmit={handleSubmit(data)}>
+          <input type="text" placeholder="nombre" 
+          {...register("nombre", {required: true})}
+          />
+          <input type="text" placeholder="direccion"
+          {...register("direccion", {required: true})}
+          />
+          <textarea placeholder="Descripción" id="" rows="3"></textarea>
+          <input type="boolean" placeholder="activo"/>
+          <button>Guardar</button>
+        </form>
+      </div>
     )
   }
   
