059cd4ccc5bb6d75b6a9c3059eed3eea6e8cbf9d
Rodrigit-hub <rodri98alves@gmail.com>
2024-09-26 19:18:51 -0300

    .

--- guardtrack_frontend/src/App.jsx
+++ guardtrack_frontend/src/App.jsx
@@ -1,20 +1,23 @@
-import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { EmpresaPage } from '../pages/EmpresaPage';
 import { EmpresaFormPage } from '../pages/EmpresaFormPage';
 import { Navigation } from '../components/Navigation';
+import { Toaster } from "react-hot-toast";
+
 
 function App() {
   return (
     <BrowserRouter>
-    <Navigation/>
-
-    
-    <Routes>
-      <Route path='/' element={<Navigate/>}/>
-      <Route path='/empresas' element={<EmpresaPage/>}/>
-      <Route path='/crear-empresa' element={<EmpresaFormPage/>}/>
-      <Route path='/empresas/:id' element={<EmpresaFormPage/>}/>
-    </Routes>
+      <div className='container mx-auto'>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Navigate />} />
+          <Route path='/empresas' element={<EmpresaPage />} />
+          <Route path='/crear-empresa' element={<EmpresaFormPage />} />
+          <Route path='/empresas/:id' element={<EmpresaFormPage />} />
+        </Routes>
+        <Toaster />
+      </div>
     </BrowserRouter>
   );
 }
